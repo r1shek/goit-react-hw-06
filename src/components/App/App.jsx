543828333e27d41f732cf9973e36a3503c37e9f5
r1shek <rostic523@gmail.com>
2024-07-24 17:55:49 +0200

    fix

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,25 +1,15 @@
-import { useSelector } from 'react-redux';
 import ContactForm from '../ContactForm/ContactForm';
 import SearchBox from '../SearchBox/SearchBox';
 import ContactList from '../ContactList/ContactList';
-import { selectContacts } from '../../redux/contactsSlice';
-import { selectNameFilter } from '../../redux/filtersSlice';
 import css from './App.module.css';
 
 export default function App() {
-  const contacts = useSelector(selectContacts);
-  const filter = useSelector(selectNameFilter);
-
-  const visibleContacts = contacts.filter((contact) =>
-    contact.name.toLowerCase().includes(filter.toLowerCase())
-  );
-
   return (
     <div className={css.phonebook}>
       <h1>Phonebook</h1>
       <ContactForm />
       <SearchBox />
-      <ContactList contactLists={visibleContacts} />
+      <ContactList />
     </div>
   );
 }
